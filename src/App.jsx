bcc43d4df144f77a932408b137b346411120ecdf
nknilandu <nknilandu@gmail.com>
2025-12-08 19:22:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      hello
+      <button className='btn btn-primary'></button>
+    </div>
+  );
+};
+
+export default App;
